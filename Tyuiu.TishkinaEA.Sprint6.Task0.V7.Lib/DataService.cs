658adef7933b2dcc2d08f1b7b90c418e81643d10
Timofey Chashkov <Tyuiu.ChashkovTA@outlook.com>
2024-12-14 01:09:49 +0500

    Add project files.

---
+++ Tyuiu.TishkinaEA.Sprint6.Task0.V7.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+
+namespace Tyuiu.TishkinaEA.Sprint6.Task0.V7.Lib
+{
+    public class DataService : ISprint6Task0V7
+    {
+        public double Calculate(int x)
+        {
+            double denominator = 2 * Math.Pow(x + 5, 2);
+
+            if (Math.Abs(denominator) < 1e-10) 
+            {
+                return 0;
+            }
+
+            double y = Math.Pow(x, 3) / denominator;
+            return Math.Round(y, 3); 
+        }
+        
+    }
+}
---
+++ Tyuiu.TishkinaEA.Sprint6.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.TishkinaEA.Sprint6.Task0.V7.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
