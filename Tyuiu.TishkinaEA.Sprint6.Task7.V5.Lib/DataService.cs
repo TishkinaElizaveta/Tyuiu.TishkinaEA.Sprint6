60267a6a89410f7a2f35b2674b1842be93cf5c5e
admin <admin@DESKTOP-LHFSSMF>
2024-12-28 08:47:01 +0500

    Task7

--- Tyuiu.TishkinaEA.Sprint6.Task7.V5.Lib/DataService.cs
+++ Tyuiu.TishkinaEA.Sprint6.Task7.V5.Lib/DataService.cs
@@ -6,87 +6,48 @@ namespace Tyuiu.TishkinaEA.Sprint6.Task7.V5.Lib
     {
         public int[,] GetMatrix(string path)
         {
-        public class MatrixCsvProcessor
+            var lines = File.ReadAllLines(path);
+            var matrix = new int[lines.Length, lines[0].Split(',').Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                var values = lines[i].Split(',').Select(int.Parse).ToArray();
+                for (int j = 0; j < values.Length; j++)
+                {
+                    matrix[i, j] = values[j];
+                }
+            }
+
+            return matrix;
+        }
+
+        // Сохранение матрицы в CSV
+        public void SaveMatrix(int[,] matrix, string path)
         {
-            /// <summary>
-            /// Загружает матрицу из CSV файла и возвращает ее в виде двумерного массива.
-            /// </summary>
-            /// <param name="path">Путь к CSV файлу.</param>
-            /// <returns>Двумерный массив целых чисел.</returns>
-            public int[,] GetMatrix(string path)
+            var lines = new string[matrix.GetLength(0)];
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                if (!File.Exists(path))
-                    throw new FileNotFoundException("Файл не найден", path);
-
-                string[] lines = File.ReadAllLines(path);
-                int rowCount = lines.Length;
-                int colCount = lines[0].Split(',').Length;
-
-                int[,] matrix = new int[rowCount, colCount];
-
-                for (int i = 0; i < rowCount; i++)
-                {
-                    string[] elements = lines[i].Split(',');
-
-                    for (int j = 0; j < colCount; j++)
-                    {
-                        if (int.TryParse(elements[j], out int value))
-                        {
-                            matrix[i, j] = value;
-                        }
-                        else
-                        {
-                            throw new FormatException($"Неверный формат данных в строке {i + 1}, столбце {j + 1}.");
-                        }
-                    }
-                }
-
-                return matrix;
+                var line = string.Join(",", Enumerable.Range(0, matrix.GetLength(1)).Select(j => matrix[i, j].ToString()));
+                lines[i] = line;
             }
 
-            /// <summary>
-            /// Изменяет значения в последнем столбце матрицы: числа меньше 2 заменяются на 2.
-            /// </summary>
-            /// <param name="matrix">Двумерный массив целых чисел.</param>
-            /// <returns>Измененная матрица.</returns>
-            public int[,] ProcessLastColumn(int[,] matrix)
-            {
-                int rowCount = matrix.GetLength(0);
-                int colCount = matrix.GetLength(1);
+            File.WriteAllLines(path, lines);
+        }
 
-                for (int i = 0; i < rowCount; i++)
+        // Обработка данных
+        public int[,] ProcessMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, matrix.GetLength(1) - 1] < 2)
                 {
-                    if (matrix[i, colCount - 1] < 2)
-                    {
-                        matrix[i, colCount - 1] = 2;
-                    }
+                    matrix[i, matrix.GetLength(1) - 1] = 2;
                 }
-
-                return matrix;
             }
+            return matrix;
+        }
 
-            /// <summary>
-            /// Сохраняет матрицу в CSV файл.
-            /// </summary>
-            /// <param name="matrix">Двумерный массив целых чисел.</param>
-            /// <param name="path">Путь к выходному CSV файлу.</param>
-            public void SaveMatrixToCsv(int[,] matrix, string path)
-            {
-                int rowCount = matrix.GetLength(0);
-                int colCount = matrix.GetLength(1);
+            }
+        }
+   
 
-                using (StreamWriter writer = new StreamWriter(path))
-                {
-                    for (int i = 0; i < rowCount; i++)
-                    {
-                        string[] row = new string[colCount];
-
-                        for (int j = 0; j < colCount; j++)
-                        {
-                            row[j] = matrix[i, j].ToString();
-                        }
-
-                        writer.WriteLine(string.Join(",", row));
-                    }
-    }
-}
